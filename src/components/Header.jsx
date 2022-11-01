f7e8e4657145ece27e59dd43211784c5ee14b682
Henrique Roos <henriquemroos21@gmail.com>
2022-11-01 16:22:32 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return ( 
+    <div className="container-header">
+      <div className="container-title">
+        <h1 className="gradient-text">Programação.</h1>
+        <h1 className="gradient-text">Tecnologia.</h1>
+        <h1 className="gradient-text">Dados.</h1>
+      </div>
+    </div>
+   );
+}
+ 
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return ( 
+    <div className="container">
+      <h5>&lt;Blog-Programando/&gt;</h5>
+    </div>
+   );
+}
+ 
+export default Navbar;
